5ec6476ab6852f489c398b411ded9cf582c75239
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-03-26 11:03:36 +0000

    added 4 questions to quiz to test and add more at the end

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "What is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false }
+        ]
+    }, 
+    {
+        question: "What is the smallest country in the world?",
+        answers: [
+            { text: "Vatican City", correct: true },
+            { text: "Bhutan", correct: false },
+            { text: "Nepal", correct: false },
+            { text: "Sri Lanka", correct: false }
+        ]
+    },
+    {
+        question: "What is the largest desert in the world?",
+        answers: [
+            { text: "Antartica", correct: true },
+            { text: "Gobi", correct: false },
+            { text: "Sahara", correct: false },
+            { text: "Kalahari", correct: false }
+        ]
+    }, 
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Europe", correct: false },
+            { text: "Africa", correct: false }
+        ]
+    },  
+]
