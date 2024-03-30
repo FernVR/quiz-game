dd7f542f62e8ba3fb00b50ceb7c0269741e6951f
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-03-30 11:36:35 +0000

    added styles and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -189,6 +189,7 @@ const questions = [
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+const scores = document.getElementsByClassName("scores-area");
 
 let currentQuestionIndex = 0;
 let score = 0;
